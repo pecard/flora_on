93dc2a39790a5a0749b946da4d432b1434de0582
pecard <pauloeducardoso@gmail.com>
2021-02-13 19:49:57 +0000

    1st summary

--- R/bio4all_fetchdata.R
+++ R/bio4all_fetchdata.R
@@ -9,8 +9,10 @@ fon <- get_inat_obs_project('flora-on')
 
 # Data.table ----
 setDT(fon)
+# Adjust classes
 fon[, created_at := ymd_hms(created_at) ] # posixct
 fon[, day := as.POSIXct(trunc(created_at, 'days')) ] # round day
+
 setkey(fon, created_at) # DT key
 
 # Summaries ----
@@ -18,40 +20,35 @@ fon[created_at > (Sys.Date() - 6)][, .(count = .N), by = day]
 fon[created_at > (Sys.Date() - 6)][, .(count = .N)]
 
 # records from the last 7 days ----
+end <- Sys.Date()
+ini <- Sys.Date() - 6
+
 fonweek <-
-  fon[created_at > (Sys.Date() - 6)][, 1:11]
+  fon[created_at %between% c(ini, end)][
+    , c('id', 'latitude', 'longitude', 'observed_on')]
 
 # Coerce to sf spatial ----
 fonweekg <-
   fonweek %>%
   st_as_sf(coords = c("longitude", "latitude"), crs = 4326, agr = "constant")
 
-# Admin data level 1 ----
-pt_adm1 <- getData('GADM', country='Portugal', level=1)
-
-# Bounding box for continental PT ----
-ptc_lim = data.frame(ylim=c(35, 43), xlim=c(-12, -6.0))
-pt_bbox <-
-  st_bbox(c(xmin=ptc_lim$xlim[1],
-            xmax=ptc_lim$xlim[2],
-            ymin=ptc_lim$ylim[1],
-            ymax=ptc_lim$ylim[2]))
-ptc_adm1 <-
-  sf::st_as_sf(pt_adm1) %>%
-  st_crop(pt_bbox)
 
 # Aggregate observations by Level1 (District)
 p_ag1 <-
-  aggregate(fonweekg, ptc_adm1['NAME_1'],
+  aggregate(fonweekg, adm1_cont['NAME_1'],
+            FUN = function(x) length(x))
+
+p_utm <-
+  aggregate(fonweekg, utm10['UTM'],
             FUN = function(x) length(x))
 
 # Plot with tmap ----
 tmap_mode("plot")
 
-p1 <- tm_shape(p_ag1) +
+p1 <- tm_shape(p_utm) +
   tm_fill(col = 'id',
           palette =  "viridis",
-          breaks = c(1, 11, 51, 150),
+          breaks = c(1, 11, 26, max(p_utm$id, na.rm = T)),
           textNA = 'Sem registo',
           border.col = "white",
           style = "fixed",
@@ -73,8 +70,8 @@ p1 <- tm_shape(p_ag1) +
     legend.position = c("right","bottom"),
     legend.bg.color = NA,
     legend.bg.alpha = 1)
-
+p1
 # Export ----
-tmap_save(p1, "D:/paulo/spbotanica/registos_semana1.png",
+tmap_save(p1, paste0("./outputs/", 'registos_semana_', ini, '_', end, '.png'),
           width=400, height=500, scale = 0.8,
           dpi = 150, asp = 0)
--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 <!-- badges: start -->
 <!-- badges: end -->
 
-The goal of this repo is to fetch data from the Flora-on Biodiversity4All project, get basic metrics and maps to communicate the progress on social media. Data will be integrated into the Flora-on Portal (http://www.flora-on.pt), the largest repository and database of vascular flora in Portugal. Flora-on is a project mantained by Sociedade Portuguesa de Botânica (http://www.spbotanica.pt).
+The goal of this repo is to fetch data from the Flora-on Biodiversity4All project with the API provided by ropensci package rinat, get basic metrics and maps to communicate the progress on social media. Data will be integrated into the Flora-on Portal (http://www.flora-on.pt), the largest repository and database of vascular flora in Portugal. Flora-on is a project mantained by Sociedade Portuguesa de Botânica (http://www.spbotanica.pt).
