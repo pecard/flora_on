ca5189c736e508c8ab087a66abc9a0841b225539
pecard <pauloeducardoso@gmail.com>
2021-02-13 21:09:19 +0000

    add data

--- R/bio4all_fetchdata.R
+++ R/bio4all_fetchdata.R
@@ -20,58 +20,31 @@ fon[created_at > (Sys.Date() - 6)][, .(count = .N), by = day]
 fon[created_at > (Sys.Date() - 6)][, .(count = .N)]
 
 # records from the last 7 days ----
-end <- Sys.Date()
 ini <- Sys.Date() - 6
+end <- Sys.Date()
 
 fonweek <-
   fon[created_at %between% c(ini, end)][
-    , c('id', 'latitude', 'longitude', 'observed_on')]
-
-# Coerce to sf spatial ----
-fonweekg <-
-  fonweek %>%
+    , c('id', 'latitude', 'longitude', 'observed_on')] %>%
   st_as_sf(coords = c("longitude", "latitude"), crs = 4326, agr = "constant")
 
 
 # Aggregate observations by Level1 (District)
 p_ag1 <-
-  aggregate(fonweekg, adm1_cont['NAME_1'],
+  aggregate(fonweek, adm1_cont['NAME_1'],
             FUN = function(x) length(x))
 
 p_utm <-
   aggregate(fonweekg, utm10['UTM'],
             FUN = function(x) length(x))
 
-# Plot with tmap ----
-tmap_mode("plot")
+ggplot() +
+  #geom_sf(data = adm1_cont, fill = NA, colour = 'grey20', alpha = .3) +
+  geom_sf(aes(fill = id), color = NA, data = p_utm) +
+  geom_sf(data = adm0_cont, fill = NA, color = 'grey80', size = .01) +
+  scale_fill_viridis_c('Registos', option = "plasma") +
+  labs(title = paste("Total de Regitos: ", sum(p_utm$id, na.rm = T)),
+       x = NULL, y = NULL) +
+  theme_void()
 
-p1 <- tm_shape(p_utm) +
-  tm_fill(col = 'id',
-          palette =  "viridis",
-          breaks = c(1, 11, 26, max(p_utm$id, na.rm = T)),
-          textNA = 'Sem registo',
-          border.col = "white",
-          style = "fixed",
-          title = "Registos por\ndistrito",
-          lwd = .01,
-          border.alpha = .2) +
-  tm_borders(
-    col = 'white',
-    lwd = .5,
-    lty = "solid",
-    alpha = .5,
-    zindex = NA,
-    group = NA
-  ) +
-  tm_style("gray") +
-  tm_layout(
-    legend.title.size = 1,
-    legend.text.size = 0.6,
-    legend.position = c("right","bottom"),
-    legend.bg.color = NA,
-    legend.bg.alpha = 1)
-p1
-# Export ----
-tmap_save(p1, paste0("./outputs/", 'registos_semana_', ini, '_', end, '.png'),
-          width=400, height=500, scale = 0.8,
-          dpi = 150, asp = 0)
+
